17a16dc1c486a15e502a093501bb5a42dc9b9b87
fochka <45799597+fochka@users.noreply.github.com>
2018-12-12 01:47:50 +0400

    Add files via upload

---
+++ EasyBank2/EasyBank/EasyBank/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyBank
+{
+    static class Calculate
+    {
+        static double Calc(int time, double percent, bool month)
+        {
+            double money = 1;
+            if (month)
+            {
+                for (int i = 0; i < time; i++)
+                {
+                    money=money*percent*0.01;
+                }
+            }
+            else
+            {
+                for(int i = 0; i < time; i+=12)
+                {
+                    money = money * percent * 0.01;
+                }
+            }
+            return (money);
+        }
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
