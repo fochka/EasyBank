4bf4fe6b15652e598fd482f60341b694266efa00
fochka <fochee@mail.ru>
2018-12-12 13:06:08 +0400

    Дизайн формы

--- EasyBank2/EasyBank/EasyBank/LoginForm.cs
+++ EasyBank2/EasyBank/EasyBank/LoginForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Design
 namespace EasyBank
 {
     public partial class LoginForm : Form
@@ -17,6 +18,6 @@ namespace EasyBank
             InitializeComponent();
         }
 
-        //win
+       
     }
 }
